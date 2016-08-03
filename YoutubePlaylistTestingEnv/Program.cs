2724b338db9acce2dfc467b62b4a87e27c39b1bd
Jack Ryder <me@jackryder.co.uk>
2016-08-03 22:20:15 +0100

    initial commit

---
+++ YoutubePlaylistTestingEnv/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+namespace YoutubePlaylistTestingEnv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            playlist playList = new playlist();
+           string[] videos = playList.getVideos("API_KEY", "PLsgo2fbJ-_T2o1dtJVl2nXFo4X-wFBD6a");
+
+            foreach(string video in videos)
+            {
+                Console.WriteLine(video);
+            }
+            Console.ReadLine();
+
+        }
+
+        
+    }
+}
